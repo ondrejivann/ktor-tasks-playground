2c4a26c2cb65652f86508e4718b4799af1705d8b
Ondřej Ivan <ondrej.ivann@gmail.com>
2025-03-07 19:29:46 +0100

    Init commit

---
+++ src/main/kotlin/Databases.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import io.ktor.server.application.*
+import org.jetbrains.exposed.sql.Database
+
+fun Application.configureDatabases() {
+    Database.connect(
+        url = "jdbc:postgresql://localhost:5432/ktor_tutorial_db",
+        user = "ktor",
+        password = "ktor",
+    )
+}
