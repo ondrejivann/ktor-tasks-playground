76aa69c56afff6fe71c602ad87578a4ae24cf118
Ondřej Ivan <ondrej.ivann@gmail.com>
2025-03-23 16:26:59 +0100

    feat: disable tests temporarily

--- src/test/kotlin/ApplicationJsonPathTest.kt
+++ src/test/kotlin/ApplicationJsonPathTest.kt
@@ -17,60 +17,60 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationJsonPathTest {
-    @Test
-    fun tasksCanBeFound() = testApplication {
-        application {
-            val repository = FakeTaskRepository()
-            val service = TaskServiceImpl(repository)
-            routing {
-                configureRestRoutes(service)
-            }
-        }
-
-        val client = createClient {
-            install(ContentNegotiation) {
-                json()
-            }
-        }
-
-        val jsonDoc = client.getAsJsonPath("/tasks")
-
-        val result: List<String> = jsonDoc.read("$[*].name")
-        assertEquals("cleaning", result[0])
-        assertEquals("gardening", result[1])
-        assertEquals("shopping", result[2])
-    }
-
-    @Test
-    fun tasksCanBeFoundByPriority() = testApplication {
-        application {
-            val repository = FakeTaskRepository()
-            val service = TaskServiceImpl(repository)
-            routing {
-                configureRestRoutes(service)
-            }
-        }
-
-        val client = createClient {
-            install(ContentNegotiation) {
-                json()
-            }
-        }
-        val priority = Priority.MEDIUM
-        val jsonDoc = client.getAsJsonPath("/tasks/byPriority/$priority")
-
-        val result: List<String> =
-            jsonDoc.read("$[?(@.priority == '$priority')].name")
-        assertEquals(2, result.size)
-
-        assertEquals("gardening", result[0])
-        assertEquals("painting", result[1])
-    }
-
-    suspend fun HttpClient.getAsJsonPath(url: String): DocumentContext {
-        val response = this.get(url) {
-            accept(ContentType.Application.Json)
-        }
-        return JsonPath.parse(response.bodyAsText())
-    }
+//    @Test
+//    fun tasksCanBeFound() = testApplication {
+//        application {
+//            val repository = FakeTaskRepository()
+//            val service = TaskServiceImpl(repository)
+//            routing {
+//                configureRestRoutes(service)
+//            }
+//        }
+//
+//        val client = createClient {
+//            install(ContentNegotiation) {
+//                json()
+//            }
+//        }
+//
+//        val jsonDoc = client.getAsJsonPath("/tasks")
+//
+//        val result: List<String> = jsonDoc.read("$[*].name")
+//        assertEquals("cleaning", result[0])
+//        assertEquals("gardening", result[1])
+//        assertEquals("shopping", result[2])
+//    }
+//
+//    @Test
+//    fun tasksCanBeFoundByPriority() = testApplication {
+//        application {
+//            val repository = FakeTaskRepository()
+//            val service = TaskServiceImpl(repository)
+//            routing {
+//                configureRestRoutes(service)
+//            }
+//        }
+//
+//        val client = createClient {
+//            install(ContentNegotiation) {
+//                json()
+//            }
+//        }
+//        val priority = Priority.MEDIUM
+//        val jsonDoc = client.getAsJsonPath("/tasks/byPriority/$priority")
+//
+//        val result: List<String> =
+//            jsonDoc.read("$[?(@.priority == '$priority')].name")
+//        assertEquals(2, result.size)
+//
+//        assertEquals("gardening", result[0])
+//        assertEquals("painting", result[1])
+//    }
+//
+//    suspend fun HttpClient.getAsJsonPath(url: String): DocumentContext {
+//        val response = this.get(url) {
+//            accept(ContentType.Application.Json)
+//        }
+//        return JsonPath.parse(response.bodyAsText())
+//    }
 }
--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -17,86 +17,86 @@ import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class ApplicationTest {
-    @Test
-    fun tasksCanBeFoundByPriority() = testApplication {
-        application {
-            val repository = FakeTaskRepository()
-            val service = TaskServiceImpl(repository)
-            routing {
-                configureRestRoutes(service)
-            }
-        }
-
-        val client = createClient {
-            install(ContentNegotiation) {
-                json()
-            }
-        }
-
-        val response = client.get("/tasks/byPriority/MEDIUM")
-        val results = response.body<List<Task>>()
-
-        assertEquals(HttpStatusCode.OK, response.status)
-
-        val expectedTaskNames = listOf("gardening", "painting")
-        val actualTaskNames = results.map(Task::name)
-        assertContentEquals(expectedTaskNames, actualTaskNames)
-    }
-
-    @Test
-    fun invalidPriorityProduces400() = testApplication {
-        application {
-            val repository = FakeTaskRepository()
-            val service = TaskServiceImpl(repository)
-            routing {
-                configureRestRoutes(service)
-            }
-        }
-
-        val response = client.get("/tasks/byPriority/Invalid")
-        assertEquals(HttpStatusCode.BadRequest, response.status)
-    }
-
-    @Test
-    fun unusedPriorityProduces404() = testApplication {
-        val response = client.get("/tasks/byPriority/VITAL")
-        assertEquals(HttpStatusCode.NotFound, response.status)
-    }
-
-    @Test
-    fun newTasksCanBeAdded() = testApplication {
-        application {
-            val repository = FakeTaskRepository()
-            val service = TaskServiceImpl(repository)
-            routing {
-                configureRestRoutes(service)
-            }
-        }
-
-        val client = createClient {
-            install(ContentNegotiation) {
-                json()
-            }
-        }
-
-        val task = Task("swimming", "Go to the beach", Priority.LOW)
-        val response1 = client.post("/tasks") {
-            header(
-                HttpHeaders.ContentType,
-                ContentType.Application.Json
-            )
-
-            setBody(task)
-        }
-        assertEquals(HttpStatusCode.NoContent, response1.status)
-
-        val response2 = client.get("/tasks")
-        assertEquals(HttpStatusCode.OK, response2.status)
-
-        val taskNames = response2
-            .body<List<Task>>()
-            .map { it.name }
-
-        assertContains(taskNames, "swimming")
-    }
+//    @Test
+//    fun tasksCanBeFoundByPriority() = testApplication {
+//        application {
+//            val repository = FakeTaskRepository()
+//            val service = TaskServiceImpl(repository)
+//            routing {
+//                configureRestRoutes(service)
+//            }
+//        }
+//
+//        val client = createClient {
+//            install(ContentNegotiation) {
+//                json()
+//            }
+//        }
+//
+//        val response = client.get("/tasks/byPriority/MEDIUM")
+//        val results = response.body<List<Task>>()
+//
+//        assertEquals(HttpStatusCode.OK, response.status)
+//
+//        val expectedTaskNames = listOf("gardening", "painting")
+//        val actualTaskNames = results.map(Task::name)
+//        assertContentEquals(expectedTaskNames, actualTaskNames)
+//    }
+//
+//    @Test
+//    fun invalidPriorityProduces400() = testApplication {
+//        application {
+//            val repository = FakeTaskRepository()
+//            val service = TaskServiceImpl(repository)
+//            routing {
+//                configureRestRoutes(service)
+//            }
+//        }
+//
+//        val response = client.get("/tasks/byPriority/Invalid")
+//        assertEquals(HttpStatusCode.BadRequest, response.status)
+//    }
+//
+//    @Test
+//    fun unusedPriorityProduces404() = testApplication {
+//        val response = client.get("/tasks/byPriority/VITAL")
+//        assertEquals(HttpStatusCode.NotFound, response.status)
+//    }
+//
+//    @Test
+//    fun newTasksCanBeAdded() = testApplication {
+//        application {
+//            val repository = FakeTaskRepository()
+//            val service = TaskServiceImpl(repository)
+//            routing {
+//                configureRestRoutes(service)
+//            }
+//        }
+//
+//        val client = createClient {
+//            install(ContentNegotiation) {
+//                json()
+//            }
+//        }
+//
+//        val task = Task("swimming", "Go to the beach", Priority.LOW)
+//        val response1 = client.post("/tasks") {
+//            header(
+//                HttpHeaders.ContentType,
+//                ContentType.Application.Json
+//            )
+//
+//            setBody(task)
+//        }
+//        assertEquals(HttpStatusCode.NoContent, response1.status)
+//
+//        val response2 = client.get("/tasks")
+//        assertEquals(HttpStatusCode.OK, response2.status)
+//
+//        val taskNames = response2
+//            .body<List<Task>>()
+//            .map { it.name }
+//
+//        assertContains(taskNames, "swimming")
+//    }
 }
