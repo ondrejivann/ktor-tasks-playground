a533376ab89f618715b8d9263f839e4f57bab843
Ondřej Ivan <ondrej.ivann@gmail.com>
2025-03-31 13:47:59 +0200

    feat: fix env properties handling

--- src/main/kotlin/config/ktor/Application.kt
+++ src/main/kotlin/config/ktor/Application.kt
@@ -1,13 +1,11 @@
 package config.ktor
 
-import io.github.cdimascio.dotenv.dotenv
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    setAWSProperties()
     embeddedServer(Netty,
         configure = {
             val cliConfig = CommandLineConfig(args)
@@ -29,9 +27,3 @@ fun Application.module() {
 }
 
 private val logger = KotlinLogging.logger {}
-
-private fun setAWSProperties() {
-    val dotenv = dotenv()
-    System.setProperty("AWS_ACCESS_KEY", dotenv["AWS_ACCESS_KEY"] ?: "")
-    System.setProperty("AWS_SECRET_KEY", dotenv["AWS_SECRET_KEY"] ?: "")
-}
